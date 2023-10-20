3010c519d8d4add6fea2524226bf4b61ad691d53
VDA <Vin-dellavv@github.com>
2023-10-20 17:26:47 +0200

    FIX fixed path error

--- resolvers.js
+++ resolvers.js
@@ -2,9 +2,9 @@ import fs from "fs";
 import { PubSub } from 'graphql-subscriptions';
 
 // get data from jsons
-const episodes = JSON.parse(fs.readFileSync('../db/Episodes.json')).elements;
-const locations = JSON.parse(fs.readFileSync('../db/Locations.json')).elements;
-const characters = JSON.parse(fs.readFileSync('../db/Characters.json')).elements;
+const episodes = JSON.parse(fs.readFileSync('./db/Episodes.json')).elements;
+const locations = JSON.parse(fs.readFileSync('./db/Locations.json')).elements;
+const characters = JSON.parse(fs.readFileSync('./db/Characters.json')).elements;
 
 const pubsub = new PubSub();
 
